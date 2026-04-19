e358371f0926b8ab7ada4d06f06d0bbde01ff7c2
xacosta <xacosta@unicesar.edu.co>
2026-04-18 22:20:02 -0500

    feat: se crearon las variables en la clase producto

--- src/Producto.java
+++ src/Producto.java
@@ -1,12 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author User
- */
 public class Producto {
     
+     private final String nombre;
+     private final double precio;
+     
+     public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    
+    
 }
+
